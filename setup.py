114a58b2a706401740fc5b1f8e27d4871efee492
BTaskaya <batuhanosmantaskaya@gmail.com>
2018-03-13 21:00:53 +0300

    some setup

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='pyBim',
     version='1.0.0',
     description='Bim Aktuel Module for Python',
     license='MIT',
-    packages=['pyBim'],
-    author='Batuhan Taskaya',
+    packages=find_packages(),
+    author='BTaskaya',
     author_email='batuhanosmantaskaya@gmail.com',
     keywords=['bim','pyBim','Bim Aktuel'],
-    url='https://github.com/btaskaya/pyBim'
+    long_description = ''.join(open('README.md','r').readlines()),
+    url='https://github.com/btaskaya/pyBim',
+    download_url = 'https://github.com/BTaskaya/pyBim/archive/v1.0.1.tar.gz',
 )
